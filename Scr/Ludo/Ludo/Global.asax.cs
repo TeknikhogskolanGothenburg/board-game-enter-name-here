83ea9a60e051939a11a067d2ef15f675d89eef1f
Z <zilvergamer@gmail.com>
2018-01-17 00:51:19 +0100

    Revert "Basic HTML initiated"
    This reverts commit 2d161eeb7f176d0e125c65d7a2d514a34352ef8e.

--- Scr/Ludo/Ludo/Global.asax.cs
+++ Scr/Ludo/Ludo/Global.asax.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-using System.Web.Optimization;
 
 namespace Ludo
 {
@@ -14,7 +13,6 @@ namespace Ludo
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
