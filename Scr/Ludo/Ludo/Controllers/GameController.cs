837d0d54b445b574bd927a7a8959780285a4f177
Z <zilvergamer@gmail.com>
2018-01-24 00:21:46 +0100

    Game View mostly done
    Game view mostly done, new ideas.
    Game board tiles numbering fixed

--- Scr/Ludo/Ludo/Controllers/GameController.cs
+++ Scr/Ludo/Ludo/Controllers/GameController.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Ludo.Models;
 
 namespace Ludo.Controllers
 {
     public class GameController : Controller
     {
         // GET: Game
+        [HttpGet]
         public ActionResult Index()
+        {
+            var name = new PlayerController();
+            return View(name);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string product)
+        {
+
+            return View();
+        }
+
+        public ActionResult Join()
+        {
+            return View();
+        }
+
+        public ActionResult New()
         {
             return View();
         }
