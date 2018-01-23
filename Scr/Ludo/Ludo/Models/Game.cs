43206dcb9780dcbb945008652efa0e187cc1d7a1
erolss <erik@eolsson.com>
2018-01-23 11:44:38 +0100

    Added gamecontroller, views, viewmodel, model

---
+++ Scr/Ludo/Ludo/Models/Game.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ludo.Models
+{
+    public class Game
+    {
+        // data för att skapa & spela ett Game, typ Game-objektet från GameEngine
+    }
+}
